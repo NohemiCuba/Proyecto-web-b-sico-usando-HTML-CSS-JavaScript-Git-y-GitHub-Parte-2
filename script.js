0e60498c2320418a6e8c4eef06aaacb2a2da1e6a
Sharit <230250@unsaac.edu.pe>
2026-05-07 00:43:11 -0500

    Se mejora interfaz visual y validaciones

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 function saludar() {
-  // Obtener el valor del input
-  let nombre = document.getElementById("nombre").value;
-  // Validar si el usuario escribió algo
-  if (nombre === "") {
-    document.getElementById("resultado").innerText =
-      "Por favor, ingresa tu nombre.";
-  } else {
-    document.getElementById("resultado").innerText =
-      "Hola " + nombre + ", bienvenido al sistema.";
-  }
+
+    let nombre = document.getElementById("nombre").value;
+
+    if(nombre === ""){
+        document.getElementById("resultado").innerText =
+        "Por favor, ingrese su nombre.";
+    }
+    else{
+        document.getElementById("resultado").innerText =
+        "Hola " + nombre + ", bienvenido al sistema.";
+    }
 }
+
 function validarCorreo() {
-  let correo = document.getElementById("correo").value;
-  if (correo === "") {
-    document.getElementById("mensajeCorreo").innerText =
-      "Debe ingresar un correo.";
-  } else {
-    document.getElementById("mensajeCorreo").innerText =
-      "Correo registrado correctamente.";
-  }
+
+    let correo = document.getElementById("correo").value;
+
+    if(correo === ""){
+        document.getElementById("mensajeCorreo").innerText =
+        "Debe ingresar un correo.";
+    }
+    else if(!correo.includes("@")){
+        document.getElementById("mensajeCorreo").innerText =
+        "Correo inválido.";
+    }
+    else{
+        document.getElementById("mensajeCorreo").innerText =
+        "Correo registrado correctamente.";
+    }
 }
